f1fa8f9f77c1aefb0c09ad24bcdc8c2cf8852f04
jcritzer <jcritzer@hawk.iit.edu>
2025-03-16 19:31:40 -0500

    reconfigured AWS secrets

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import groupRoutes from './routes/groupRoutes';
 import trackRoutes from './routes/trackRoutes';
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+import configSecrets from './secrets';
 
-// Authentication routes
-app.use('/api/auth', authRoutes);
+async function initializeApp() {
+    await configSecrets();
 
-// Group routes
-app.use('/api/groups', groupRoutes);
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
 
-// Track routes
-app.use('/api/tracks', trackRoutes);
+    // Authentication routes
+    app.use('/api/auth', authRoutes);
 
-console.log(`${process.env.PORT}`)
+    // Group routes
+    app.use('/api/groups', groupRoutes);
 
-const PORT = Number(process.env.PORT) || 5000;
-app.listen(PORT, "0.0.0.0", () => { 
-    console.log("Server is running on port 5000 and accessible externally");
-});
+    // Track routes
+    app.use('/api/tracks', trackRoutes);
+
+    console.log(`${process.env.PORT}`)
+
+    const PORT = Number(process.env.PORT) || 5000;
+    app.listen(PORT, "0.0.0.0", () => { 
+        console.log("Server is running on port 5000 and accessible externally");
+    })
+}
+
+initializeApp().catch((err) => {
+    console.error("Failed to initialize secrets:" , err);
+    process.exit(1);
+})
