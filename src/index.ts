c73c5743eed702d82e6c5d062ef6417b5e33fb81
jcritzer <jcritzer@hawk.iit.edu>
2025-03-25 21:07:32 -0500

    Add printing for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,3 @@
-/* import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import groupRoutes from './routes/groupRoutes';
-import trackRoutes from './routes/trackRoutes';
-
-import configSecrets from './secrets';
-
-async function initializeApp() {
-    await configSecrets();
-
-    const { PrismaClient } = await import("@prisma/client"); // ✅ Load Prisma only after secrets are set
-
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
-
-    // Authentication routes
-    app.use('/api/auth', authRoutes);
-
-    // Group routes
-    app.use('/api/groups', groupRoutes);
-
-    // Track routes
-    app.use('/api/tracks', trackRoutes);
-
-    console.log(`${process.env.PORT}`)
-
-    const PORT = Number(process.env.PORT) || 5000;
-    app.listen(PORT, "0.0.0.0", () => { 
-        console.log("Server is running on port 5000 and accessible externally");
-    })
-}
-
-initializeApp().catch((err) => {
-    console.error("Failed to initialize secrets:" , err);
-    process.exit(1);
-}) */
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -56,7 +17,7 @@ async function start() {
     process.env.AWS_REGION = await getSSMParameter("/beatsync/AWS_REGION");
     process.env.S3_BUCKET_NAME = await getSSMParameter("/beatsync/S3_BUCKET_NAME");
 
-    console.log(`Secrets loaded successfully`)
+    console.log(`Secrets loaded successfully DEMO_MODE=${DEMO_MODE}, BUCKET_NAME=${process.env.S3_BUCKET_NAME}`)
 
     // Now that secrets are loaded, you can import your routes
     const authRoutes = (await import("./routes/authRoutes")).default;
